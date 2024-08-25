f46cfc57fdb7fcf8155dc1d63b6f150f3a8da27a
Lirim Veliu <lirim@veliu.net>
2024-08-25 22:37:46 +0200

    Removed pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    "/": { prerender: true },
+  },
   experimental: { asyncContext: true },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL,
     },
   },
-  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/eslint", "@vite-pwa/nuxt"],
+  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/eslint"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   typescript: {
