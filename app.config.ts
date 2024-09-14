df4d6e3f993e0d57cd2873ebb1657eb271e5e2da
Lirim Veliu <lirim@veliu.net>
2024-09-14 19:38:34 +0200

    Reverted app config

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,6 @@ export default defineAppConfig({
   ui: {
     notifications: {
       position: "top-0 right-0",
-    },
-    notification: {
-      default: {
-        icon: 'i-heroicons-information-circle',
-        timeout: 2000
-      }
     }
-  },
+  }
 });
